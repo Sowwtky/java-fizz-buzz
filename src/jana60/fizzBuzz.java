c470587361f01610ef4024d699fc8722b1bd15ac
Sowwtky <miche46ale@gmail.com>
2022-06-09 14:10:10 +0200

    esercizio completo (no bonus)

--- src/jana60/fizzBuzz.java
+++ src/jana60/fizzBuzz.java
@@ -3,8 +3,20 @@ package jana60;
 public class fizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		for (int i = 1; i <= 100; i++) {
+			if ((i % 3) == 0 && (i % 5) == 0) {
+				System.out.println("Fizz Buzz");
+			} else if ((i % 5) == 0) {
+				System.out.println("Buzz");
+			} else if ((i % 3) == 0) {
+				System.out.println("Fizz");
+			} else {
+				System.out.println(i);
+			}
+		}
 
 	}
-
 }
+
